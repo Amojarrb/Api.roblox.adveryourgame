f540e56f33ff1e1d7717ae5d02390803ba4c2c53
Amojarrb <saul.moya@outlook.com>
2024-07-07 20:05:28 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Obtener el sufijo de la URL
-    const url = window.location.href;
-    const suffix = url.split('/').pop();
-
-    // Mostrar el sufijo en el elemento con id 'suffix'
-    document.getElementById('suffix').textContent = suffix;
-});
